68277b7be9ba04b53259300e5308f112ec194675
Sara Garcia Gallardo <saragarciagallardo@gmail.com>
2024-07-23 17:54:41 +0100

    changes in the steps to follow in main.js

--- main.js
+++ main.js
@@ -10,10 +10,30 @@ let stringRequested = prompt(
 console.log('You entered: ', stringRequested);
 
 utility.countCharacters(stringRequested);
-//Count how many times is the character in the word
-        // Elegir cada letra con For loop y de alguna forma hacerla un key o una variable
-            //Maybe using destructuring? and for in loop??
-        // For loop with if statement (si la letra es igual a la siguiente sumar
-        // +1 al sum, dejar sum y pasar al siguiente)
-//Compared them to choose the largest
-//Return the character with the largest number
+
+// IDEAS DE COMO RESOLVERLO
+
+// 1. Iterar por cada letra de la palabra.
+    // HOW: Quizas usando algun tipo de loop
+    // para inspirarme: 
+    /* for (const value of iterator) {
+       /*const source = [1, 2, 3];
+
+        const iterator = source[Symbol.iterator]();
+
+        for (const value of iterator) {
+          console.log(value);
+          if (value === 1) {
+          break;
+          }
+        console.log('This string will not be logged.');
+       }*/
+
+// 2. Crear un key o una variable con el valor de esa letra.
+    // HOW: destructuring? and for in loop??
+
+// 3. Comprobar cuantas veces esta esa letra en la palabra.
+    // 3.1 La letra objetivo se compara con el resto de las letras de la palabra.
+    // Si es igual se suma 1.
+    // Si no es igual se suma 0 o se continua.
+// 4. Comprobar los valores de cada letra y elegir el mas alto.
