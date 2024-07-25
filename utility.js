472818667f22e95cad28aa3a3995701aa55b8ef4
Sara Garcia Gallardo <saragarciagallardo@gmail.com>
2024-07-25 21:35:36 +0100

    changes added in utility.js

--- utility.js
+++ utility.js
@@ -1,13 +1,20 @@
 function countCharacters(stringRequested) {
-  console.log(stringRequested);
+  for (i = 0; i < stringRequested.length; i++) {
+    console.log("inside loop");
+    let sum = 0;
+    let positionInWord = 0;
+    let character = stringRequested[i];
+    
+    console.log(stringRequested[i], character, positionInWord);
 
-  for (const character of stringRequested) {
-    let [0] = [character];
-    if(character){
-      [character] += 1;
-      console.log([character], character);
-    } continue;
+    if (character === stringRequested[positionInWord]) {
+      sum += 1;
+      positionInWord += 1;
+      console.log(character, sum);
+      continue;
+    }
+    continue;
   }
 }
 
-module.exports = {countCharacters};
+module.exports = { countCharacters };
