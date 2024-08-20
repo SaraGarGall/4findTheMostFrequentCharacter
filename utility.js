53841e082f737c3c06bb2ad4385f51dec89ce2b1
Sara Garcia Gallardo <saragarciagallardo@gmail.com>
2024-08-20 16:11:15 +0100

    error detected in the if statement inside the for in loop

--- utility.js
+++ utility.js
@@ -1,7 +1,7 @@
 function countCharacters(stringRequested) {
   let result = {};
+  //let repetitions = 0;
   let moreFrequentCharacter = 0;
-  let repetitions;
 
   for(i = 0; i < stringRequested.length; i++){
     let key = stringRequested[i];
@@ -12,14 +12,20 @@ function countCharacters(stringRequested) {
     }
   }
 
-// Hay un problema en este for loop:
-
-  for(const key in result){
+  for (const key in result) {
+    let repetitions;
+    // El error esta en el if statement
     if (result[key] > repetitions) {
       repetitions = result[key];
-      moreFrequentCharacter = result["key"];
+      console.log('repetitions in if statement', repetitions);
+      moreFrequentCharacter = result['key'];
+      console.log(
+        'moreFrequentCharacter in if statement',
+        moreFrequentCharacter
+      );
     }
   }
+
 console.log('the result is:', result);
 console.log("The most frequent character in", stringRequested, "is:", moreFrequentCharacter);
 
