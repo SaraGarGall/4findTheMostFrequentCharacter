f1d689629962fe53e0c7d00adda170efac3bbe5e
Sara Garcia Gallardo <saragarciagallardo@gmail.com>
2024-08-20 11:48:56 +0100

    changes after the lesson

--- main.js
+++ main.js
@@ -10,34 +10,3 @@ let stringRequested = prompt(
 console.log('You entered: ', stringRequested);
 
 utility.countCharacters(stringRequested);
-
-// IDEAS DE COMO RESOLVERLO
-
-// 1. Iterar por cada letra de la palabra.
-    // HOW: Quizas usando algun tipo de loop
-    // para inspirarme: 
-    /* for (const value of iterator) {
-       /*const source = [1, 2, 3];
-
-        const iterator = source[Symbol.iterator]();
-
-        for (const value of iterator) {
-          console.log(value);
-          if (value === 1) {
-          break;
-          }
-        console.log('This string will not be logged.');
-       }*/
-
-// 2. Crear un key o una variable con el valor de esa letra.
-    // HOW: destructuring? and for in loop??
-    // Quizas una vez se inicia el loop se puede imprimir una variable que tome
-    // el valor del value del loop
-    // despues darle el valor final de esa letra
-    // o quizas hacer este paso al final del loop?
-
-// 3. Comprobar cuantas veces esta esa letra en la palabra.
-    // 3.1 La letra objetivo se compara con el resto de las letras de la palabra.
-    // Si es igual se suma 1.
-    // Si no es igual se suma 0 o se continua.
-// 4. Comprobar los valores de cada letra y elegir el mas alto.
--- utility.js
+++ utility.js
@@ -1,40 +1,5 @@
 function countCharacters(stringRequested) {
   
-  for (i = 0; i < stringRequested.length; i++) {
-    console.log('inside of the first for loop');
-    console.log('initialCharacter:', stringRequested[i]);
-    let sum = 0;
-    let maxAmount = 0;
-
-    for (j = 0; j < stringRequested.length; j++) {
-      console.log('inside of the second for loop');
-    
-      if (stringRequested[i] === stringRequested[j]) {
-        sum += 1;
-        console.log('characterCompared:', stringRequested[j], sum);
-      } else {
-        sum += 0;
-      }
-    }
-    // como poner como el nombre de la variable el valor de stringRequested[i]?
-    // y despues como atribuirle a esa variab;e el valor de sum?
-    let character = sum;
-    console.log(stringRequested[i], sum, "characterSum:", character);
-  }
-  
 }
 
 module.exports = { countCharacters };
-
-
-/*Encontrar la forma de almacenar la letra con el sum correspondiente*/
-/*Despues comparar y si el sum de la letra actual es menor que el que esta almacenado, entonces 
-se pasa a la siguiente. Si es igual o mayor, se almacena como la mas frecuente por ahora*/ 
-
-/*IDEAS PARA CONTINUAR Y PROBAR
-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Symbol.iterator
-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/valueOf
-*/
